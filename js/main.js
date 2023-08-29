443c4924c1dfa972fad20e9da893d4d33e9008e4
Iulian Laca <lacaiulian@gmail.com>
2023-08-29 08:36:05 +0300

    added carousel to page

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ $( document ).ready(function() {
     $(".hamburger-menu").toggleClass('change');
   });
 
+  // Slider/Carousel
+  $('.app-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+  });
+
 });
