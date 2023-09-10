ad28cd302604f42fa447fe5ed401eda80d137190
Iulian Laca <lacaiulian@gmail.com>
2023-09-10 18:30:54 +0300

    Added two more sections to page

--- js/main.js
+++ js/main.js
@@ -13,6 +13,34 @@ $( document ).ready(function() {
     // autoplay: true,
     autoplaySpeed: 1500,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
 });
